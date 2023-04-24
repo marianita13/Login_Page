ad5b4d17b82e8ac8f0fd4837abe70c99b6aadc49
Jonathan <jcarrillo366@misena.edu.co>
2023-04-24 09:54:00 -0500

    almost there

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+import {usuarios} from './users.js';
+// console.log(usuarios)
+
+const icono = document.getElementById('icon-eye')
+const login = document.getElementById('login')
+const user = document.getElementById('user')
+const password = document.getElementById('pass')
+const pantalla = document.getElementById('texto')
+
+icono.addEventListener('click',() => {
+    if (password.type=="text"){
+        password.type="password"
+    }else{
+        password.type="text"
+    }
+})
+
+login.addEventListener('click', (evento)=>{
+    let existe=false
+    evento.preventDefault()
+    usuarios.forEach(element => {
+        if (user.value==element.username){
+            if (password.value==element.userpass){
+                existe=true
+            }
+        }
+    });
+    if (existe==false){
+        pantalla.innerHTML="Error de usuario o contraseña"
+        setTimeout(()=>{
+            pantalla.innerHTML=('')
+        },2000)
+    }else if (existe==true){
+        user.value=''
+        password.value=''
+        location.assign("../HTML/principal.html")
+    }
+})
---
+++ JS/users.js
@@ -0,0 +1,11 @@
+export const usuarios =[
+    {nombre:"Mariana", username:"marianita13",
+        userpass:"mariana01"
+    },
+    {nombre:"Javier", username:"javier1013", 
+        userpass:"javier05"
+    },
+    {nombre:"Pepito", username:"pepito123",
+        userpass:"123456"
+    }
+]
